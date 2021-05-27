813a83fd9470c09f878eaec22953a79f5063cac5
Ayaz Rahman <muhtasim_ayaz@yahoo.com>
2021-05-27 23:35:46 +1000

    Initial Commit

---
+++ EssentialFeedLoader/EssentialFeedLoader.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedLoader.h
+//  EssentialFeedLoader
+//
+//  Created by Ayaz Rahman on 27/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedLoader.
+FOUNDATION_EXPORT double EssentialFeedLoaderVersionNumber;
+
+//! Project version string for EssentialFeedLoader.
+FOUNDATION_EXPORT const unsigned char EssentialFeedLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedLoader/PublicHeader.h>
+
+
