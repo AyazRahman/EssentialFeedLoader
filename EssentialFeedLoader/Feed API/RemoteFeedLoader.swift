e8b3ac86ac4b4ed98b5c77f4f6e31561f8ee9998
AyazRahman <muhtasim.ayaz@gmail.com>
2021-06-03 01:48:34 +1000

    Delivers invalidData error on non400 case

--- EssentialFeedLoader/Feed API/RemoteFeedLoader.swift
+++ EssentialFeedLoader/Feed API/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // Turn abstract classes into protocol
 public protocol HTTPClient{
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -18,6 +18,7 @@ public final class RemoteFeedLoader {
     
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
     
     public init(url: URL, client: HTTPClient) {
@@ -28,8 +29,13 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping (Error) -> Void){
         //avoid using shared replace with injection.
         
-        client.get(from: url) { error in
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            if let _ = response {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
+            
         }
     }
 }
